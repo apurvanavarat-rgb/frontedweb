f61c64c8eca4a223b93dcedf4aefb3df51966aec
apurvanavarat-rgb <apurvanavarat@gmail.com>
2026-07-06 17:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ function login() {
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if (username === "" || password === "") {
-        alert("Please enter Username and Password.");
+    if (username === "") {
+        alert("Please enter User ID");
+        return;
+    }
+
+    if (password === "") {
+        alert("Please enter Password");
         return;
     }
 
     if (username === "admin" && password === "admin123") {
-        alert("Login Successful!");
         window.location.href = "dashboard.html";
     } else {
-        alert("Invalid Username or Password!");
+        alert("Invalid User ID or Password");
     }
-
 }
